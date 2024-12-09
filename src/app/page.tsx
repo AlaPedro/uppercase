0e6d198b69516dce3e520fcbca37398b176bc222
Alã Pedro <alanpedro94@gmail.com>
2024-12-09 18:50:46 -0300

    feet: uppercase automatico sem button + favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,10 @@ import { useState } from 'react'
 
 export default function Home() {
     const [text, setText] = useState<string>('')
-    const uppercaseMusic = () => {
-        const textUpercase = text.toUpperCase()
-        setText(textUpercase)
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const textUppercase = event.target.value.toUpperCase()
+        setText(textUppercase)
     }
-
     const handleLineCopy = (line: string) => {
         navigator.clipboard.writeText(line)
         console.log(`Linha copiada: ${line}`)
@@ -20,30 +19,27 @@ export default function Home() {
             <h1 className="text-white text-4xl drop-shadow-lg font-semibold">
                 Uppercase cante karaokê
             </h1>
-            <div className="w-full flex flex-col items-center">
+            <div className="w-full flex flex-col items-center gap-10">
                 <textarea
                     placeholder="Copie a letra da música aqui!"
                     value={text}
-                    onChange={(e) => setText(e.target.value)}
+                    onChange={handleChange}
                     className="w-4/5 min-h-[400px] shadow-xl outline-none p-2"
                     name="uppercase"
                     id="uppercase"
                 ></textarea>
-                {lines.map((line, index) => (
-                    <p
-                        key={index}
-                        className="cursor-pointer bg-gray-100 p-2 hover:bg-purple-100 w-4/5"
-                        onClick={() => handleLineCopy(line)}
-                    >
-                        {line || ''}
-                    </p>
-                ))}
-                <button
-                    onClick={uppercaseMusic}
-                    className="text-white font-semibold bg-orange-500 w-4/5 h-12 rounded-br-md rounded-bl-md shadow-xl hover:bg-orange-600 hover:text-black transition-colors"
-                >
-                    UPPERCASE
-                </button>
+                <div className="w-4/5">
+                    <span className="text-white ">Letra para copiar</span>
+                    {lines.map((line, index) => (
+                        <p
+                            key={index}
+                            className="cursor-pointer bg-gray-100 p-2 hover:bg-purple-600 hover:text-white"
+                            onClick={() => handleLineCopy(line)}
+                        >
+                            {line || ''}
+                        </p>
+                    ))}
+                </div>
             </div>
         </div>
     )
